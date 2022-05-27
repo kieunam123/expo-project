f1bc925f508d449dcff16495a890c9ace544290b
kieunam123 <kieunam011@gmail.com>
2022-05-27 17:05:18 +0700

    updated by me

--- components/Header.js
+++ components/Header.js
@@ -9,12 +9,24 @@ import {
   } from 'react-native';
 import React, {Component} from 'react'
 import { FormatFont } from '../constants';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function Header(props){
-    const {name}=props
-    return(
+export default function Header(props) {
+    const { name,
+        leftIconName = 'ios-chevron-back',
+        rightIconName = 'ios-search',
+        onPressLeftIcon,
+        onPressRightIcon,
+    } = props
+    return (
         <SafeAreaView style={styles.container}>
+            <Icon style={styles.iconleft}
+                name={leftIconName}
+                size={35}/>
             <Text style={styles.name}>{name}</Text>
+            <Icon style={styles.iconright}
+                name={rightIconName}
+                size={30}/>
         </SafeAreaView>
     );
 }
@@ -22,17 +34,27 @@ export default function Header(props){
 const styles = StyleSheet.create({
     container:{
         height:100,
-        alignItems:'center',
-        justifyContent:'center',
         backgroundColor:'pink',
-        
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
     },
     name:{
         color:'white',
         fontSize:FormatFont.h2,
-        textTransform:'Capitalize',
+        textTransform:'capitalize',
         paddingBottom:10,
+        
 
+    },
+    iconleft:{
+        color:'white',
+        paddingHorizontal:20,
+
+    },
+    iconright:{
+        paddingHorizontal:20,
+        color:'white'
     }
 
 })
--- screens/Chat.js
+++ screens/Chat.js
@@ -7,14 +7,26 @@ import {
   Text,
   useColorScheme,
   View,
+  FlatList,
 } from 'react-native';
 import {images,icons,FormatFont} from '../constants';
 import {Buttons, Header} from '../components';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function Chat(props){
+    const [chatHistory,setChatHistory]
     return(
         <View style={styles.container}>
             <Header name={'Chat'}/>
+            <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+                <Text style={styles.unread}>7 tin nhắn chưa đọc</Text>
+                <Icon name={'trash-alt'} style={styles.unread} size={16} onPress={()=>{
+                    alert(`you pressed delete`)
+                }} />
+            </View>
+            <FlatList style={styles.list}>
+
+            </FlatList>
         </View>
     );
 
@@ -22,8 +34,15 @@ export default function Chat(props){
 
 const styles = StyleSheet.create({
     container:{
+        justifyContent: 'center',
+        
+    },
+    unread:{
+        padding:15,
+    },
+    list:{
         flex:1,
-        flexDirection:'column',
+
     }
 
 })
